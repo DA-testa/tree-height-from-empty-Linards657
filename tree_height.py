b2f684a285eef82edbc11f9a89dafcac8f764076
Linards Naglis <125291166+Linards657@users.noreply.github.com>
2023-02-21 23:45:42 +0000

    Push2

--- tree_height.py
+++ tree_height.py
@@ -2,42 +2,50 @@
 
 import sys
 import threading
-import numpy as np
 import re
+import numpy as np
+
+
+def iteration(i, parents, root):
+    global newMax
+    backlog = np.where(parents == root)[0]
+    if backlog.any():
+        i = i + 1
+        for x in range(len(backlog)):
+            if int(iteration(i, parents, backlog[x]) or 0) > newMax:
+                newMax = iteration(i, parents, backlog[x])
+    else:
+        return i
+
 
 def compute_height(n, parents):
     # Write this function
-    backlog = []
-    max_height = 0
+    max_height = 1
     lastRemembered = 0
     for i in range(parents.size):
         if -1 == parents[i]:
             root = i
-    for i in range(int(n)-1):
-        backlogbool = list(map(lambda xx: xx == root, parents))
-        for x in range(len(backlogbool)):
-            if backlogbool[x]:
-                backlog.append(x)
-        if len(backlog) >= lastRemembered:
-            max_height = max_height + 1
-            lastRemembered = len(backlog)
-        root = backlog.pop()
+    iteration(max_height, parents, root)
     # Your code here
-    return max_height
+    return newMax
 
 
 def main():
     # implement input form keyboard and from files
+    global newMax
+    newMax = 0
     text = input()
     if "I" in text[:1]:
         count = input()
         array = np.asarray(list(map(int, (input().split()))))
         print(compute_height(count, array))
     else:
+        text = input()
         if not re.search("^a", text):
-            f = open(".\\" + text, "r")
+            f = open("./test/" + text, "r")
             count = f.readline()
-            array = np.asarray(list(map(int, (f.readline.split()))))
+            array = np.asarray(list(map(int, (f.readline().split()))))
+            f.close()
             print(compute_height(count, array))
 
     # let user input file name to use, don't allow file names with letter a
